26b6d569cfbc81a5279689118d79b9cefc863a1d
alexelec <alexelecv5@gmail.com>
2018-02-23 15:35:17 +0200

    vdr-plugins: add vdr-plugin-iptv-0fe1cb7

---
+++ packages/xmedia/tvservice/vdr-plugins/vdr-plugin-iptv/package.mk
@@ -0,0 +1,36 @@
+################################################################################
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
+################################################################################
+
+PKG_NAME="vdr-plugin-iptv"
+PKG_VERSION="0fe1cb7"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.saunalahti.fi/~rahrenbe/vdr/iptv/"
+PKG_SITE="https://github.com/rofafor/vdr-plugin-iptv"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain vdr curl"
+PKG_SECTION="xmedia/tvservice"
+PKG_SHORTDESC="vdr-iptv: an IPTV plugin for the Video Disk Recorder (VDR)"
+PKG_LONGDESC="vdr-iptv is an IPTV plugin for the Video Disk Recorder (VDR)"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+PKG_LOCALE_INSTALL="yes"
+
+make_target() {
+  : not
+}
+
+makeinstall_target() {
+  make LIBDIR=/usr/lib/vdr \
+       DESTDIR=$INSTALL \
+       install
+}
+
+post_makeinstall_target() {
+  mkdir -p $INSTALL/usr/config
+    mv -f $INSTALL/storage/.config/vdr $INSTALL/usr/config
+  rm -rf $INSTALL/storage
+}
