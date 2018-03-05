1b350d7952c0de7318cd6721669aaebe3ade2b6a
alexelec <alexelecv5@gmail.com>
2018-03-05 10:12:56 +0200

    add media_build CrazyCat dvb-drivers (20180305 git:43daeda)

--- packages/xmedia/dvb/media_build/package.mk
+++ packages/xmedia/dvb/media_build/package.mk
@@ -4,15 +4,17 @@
 ################################################################################
 
 PKG_NAME="media_build"
-PKG_VERSION="2017-04-17-rpi"
+PKG_VERSION="20180305"
+GIT_VERSION="43daeda"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
+PKG_SITE="https://bitbucket.org/CrazyCat/media_build"
 PKG_SITE="https://github.com/crazycat69/linux_media"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="$ALEXELEC_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_BUILD_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_SECTION="driver"
+PKG_SECTION="xmedia/dvb"
 PKG_SHORTDESC="DVB drivers that replace the version shipped with the kernel"
 PKG_LONGDESC="DVB drivers that replace the version shipped with the kernel"
 PKG_IS_ADDON="no"
@@ -24,24 +26,14 @@ pre_make_target() {
 }
 
 make_target() {
-  make untar
-
-  # copy config file
-  if [ "$PROJECT" = PC ]; then
-    if [ -f $PKG_DIR/config/generic.config ]; then
-      cp $PKG_DIR/config/generic.config v4l/.config
-    fi
-  else
-    if [ -f $PKG_DIR/config/usb.config ]; then
-      cp $PKG_DIR/config/usb.config v4l/.config
-    fi
-  fi
-
-  # add menuconfig to edit .config
+  make VER=$KERNEL_VER SRCDIR=$(kernel_path) stagingconfig
+  # Geniatech T220A
+  sed -i -e "s|^.*CONFIG_DVB_CXD2820R.*$|CONFIG_DVB_CXD2820R=m|" "v4l/.config"
   make VER=$KERNEL_VER SRCDIR=$(kernel_path)
 }
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/modules/$KERNEL_VER/updates
   find $PKG_BUILD/v4l/ -name \*.ko -exec cp {} $INSTALL/usr/lib/modules/$KERNEL_VER/updates \;
+  echo "Media_Build CrazyCat drivers version: $PKG_VERSION~git:$GIT_VERSION" > $INSTALL/usr/lib/modules/$KERNEL_VER/updates/01-version.txt
 }
