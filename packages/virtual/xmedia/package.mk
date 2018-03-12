a580c8a85420b4c60cd2133e9c1f61cd2a7fdbc0
alexelec <alexelecv5@gmail.com>
2018-03-12 14:17:40 +0200

    ImageMagick: 7.0.7-24 & VDR fonts

--- packages/virtual/xmedia/package.mk
+++ packages/virtual/xmedia/package.mk
@@ -17,7 +17,7 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 # tools
-PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mc scan-m3u scan-s2 wget binutils"
+PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mc scan-m3u scan-s2 wget binutils ImageMagick"
 
 # tv-services
 PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET oscam vdr"
