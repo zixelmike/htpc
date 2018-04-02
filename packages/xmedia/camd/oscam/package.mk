f4be242d713d65f29f1a18d56ada45b3c5765293
alexelec <alexelecv5@gmail.com>
2018-04-02 10:22:56 +0300

    oscam: bump to 11401

--- packages/xmedia/camd/oscam/package.mk
+++ packages/xmedia/camd/oscam/package.mk
@@ -4,8 +4,8 @@
 ################################################################################
 
 PKG_NAME="oscam"
-PKG_VERSION="26ad530"
-PKG_VERSION_NUMBER="11400"
+PKG_VERSION="f783daf"
+PKG_VERSION_NUMBER="11401"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.streamboard.tv/oscam/wiki"
