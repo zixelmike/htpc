fdb5c73f2a175c0978d687ea8aa65a05d9ef7fb7
alexelec <alexelecv5@gmail.com>
2018-04-05 10:43:31 +0300

    games: add pkg retroarch-joypad-autoconfig

--- packages/xmedia/games/retroarch-joypad-autoconfig/package.mk
+++ packages/xmedia/games/retroarch-joypad-autoconfig/package.mk
@@ -3,25 +3,28 @@
 #      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
-PKG_NAME="core-info"
-PKG_VERSION="e61d38e"
+PKG_NAME="retroarch-joypad-autoconfig"
+PKG_VERSION="6547e95"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/libretro/libretro-super"
+PKG_SITE="https://github.com/libretro/retroarch-joypad-autoconfig"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="libretro-super*"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="xmedia/games"
-PKG_SHORTDESC="Info files for libretro cores"
-PKG_LONGDESC="Super repo for other libretro projects. Fetches, builds and installs."
+PKG_SHORTDESC="RetroArch joypad autoconfig files"
+PKG_LONGDESC="RetroArch joypad autoconfig files"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+post_unpack() {
+  rm -f $PKG_BUILD/configure $PKG_BUILD/Makefile
+}
 
 make_target() {
   : none
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp dist/info/*.info $INSTALL/usr/lib/libretro/
+  mkdir -p $INSTALL/usr/share/retroarch/joypad-autoconfig
+    cp -r * $INSTALL/usr/share/retroarch/joypad-autoconfig
 }
