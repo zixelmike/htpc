620d76e687245bcdb72633dce0b5fe6367f3e1aa
alexelec <alexelecv5@gmail.com>
2018-04-16 17:39:23 +0300

    jansson: add package

---
+++ packages/xmedia/depends/jansson/package.mk
@@ -0,0 +1,17 @@
+################################################################################
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
+################################################################################
+
+PKG_NAME="jansson"
+PKG_VERSION="2.7"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.digip.org/jansson/"
+PKG_URL="http://www.digip.org/$PKG_NAME/releases/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="xmedia/depends"
+PKG_SHORTDESC="Jansson is a C library for encoding, decoding and manipulating JSON data"
+PKG_LONGDESC="Jansson is a C library for encoding, decoding and manipulating JSON data"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
